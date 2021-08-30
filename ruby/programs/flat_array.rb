ecc052b8852559c5651d982465bc1bf733d1de7d
bhanu-atwork <89567782+bhanu-atwork@users.noreply.github.com>
2021-08-30 13:01:21 +0530

    Add inbuilt array function having same change

--- ruby/programs/flat_array.rb
+++ ruby/programs/flat_array.rb
@@ -1,7 +1,10 @@
 # Write a program to flat a nested array
+arr = [[1,2], [3,4]]
 def flat_array(input)
   res = []
   input.each {|ele| ele.each {|indv_ele| res.push(indv_ele)} }
 end
 
-puts flat_array([[1,2], [3,4]])
+puts flat_array(arr)
+# Inbuilt array function having same functionality
+puts arr.flatten
