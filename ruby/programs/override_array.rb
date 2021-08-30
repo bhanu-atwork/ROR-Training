8d739824bdc0c8d41bc144d83b909581c7682270
bhanu-atwork <89567782+bhanu-atwork@users.noreply.github.com>
2021-08-30 13:04:39 +0530

    add program to override array method.

--- ruby/programs/override_array.rb
+++ ruby/programs/override_array.rb
@@ -1,1 +1,9 @@
+# Write a program to double each values of an array declaring array double each function in array class
+module Enumerable
+  def double_each
+    self.map {|i| i*2}
+  end
+end
 
+nums = Array.new(5) { |e| e = e * 2 }
+puts nums.double_each
